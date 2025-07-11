adc67341ac1bdb6cb5e6755c8ea746816522c2fb
Vikas Sukhwani <v.sukhwani@gmail.com>
2025-07-11 16:42:02 +0530

    Simplify vercel.json and add test API endpoint

---
+++ api/test.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        
+        response = {
+            'status': 'OK',
+            'message': 'Test API is working',
+            'python_version': 'Available'
+        }
+        
+        self.wfile.write(json.dumps(response).encode())
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
