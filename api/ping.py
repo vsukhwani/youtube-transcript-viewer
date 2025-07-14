1173f59dc99674d0a94057e307547d1d8d4e4d97
Vikas Sukhwani <v.sukhwani@gmail.com>
2025-07-14 20:08:12 +0530

    Fixing API issues

---
+++ api/ping.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        
+        response = {
+            'message': 'pong',
+            'status': 'working',
+            'timestamp': '2025-07-14'
+        }
+        
+        self.wfile.write(json.dumps(response).encode())
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, X-API-Key')
+        self.end_headers()
